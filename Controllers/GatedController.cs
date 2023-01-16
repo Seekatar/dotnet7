d6eacbe3d3bf8fec6df7ecff477a8523cfc1c5d5
Jim Wallace <seekatar@gmail.com>
2023-01-16 09:11:34 -0500

    add gated (and controllers)

---
+++ Controllers/GatedController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement.Mvc;
+
+namespace dotnet7.Controllers
+{
+    public class GatedController : Controller
+    {
+        [HttpGet]
+        [Route("/gated")]
+        [FeatureGate("CNTXT.KEYB")]
+        public ActionResult<string> Index()
+        {
+            return "This is gated";
+        }
+    }
+}
