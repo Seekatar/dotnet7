9c6426154ccbff86fd3567a0806ba66452628020
Jim W <jim.wallace@loyalhealth.com>
2023-11-17 07:32:35 -0500

    Add variable

--- Model/AppUserPartial.cs
+++ Model/AppUserPartial.cs
@@ -1,5 +1,8 @@
 ﻿namespace Model;
 
+// dotnet pack ./Model.csproj -o /Users/jwallace/other-code/Tools-DotNet/packages -p:Packing=Packaging -p:version=1.1.1
+// or in csproj to exclude to file entirely
+// dotnet pack Model.csproj --configuration Release --output /nupkgs -p:MODELPACK=true -p:Version=1.1.1
 #if !Packaging
 
 interface IAuditable
