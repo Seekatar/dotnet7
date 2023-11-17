a94907160d15f103a189ebf28656456970552d5c
Jim Wallace <seekatar@gmail.com>
2023-11-16 21:15:47 -0500

    add test

---
+++ Model/AppUserPartial.cs
@@ -0,0 +1,15 @@
+﻿namespace Model;
+
+#if !Packaging
+
+interface IAuditable
+{
+    string AuditName { get; set; }
+}
+
+public partial class AppUser : IAuditable
+{
+    public string AuditName { get; set; } = "Hi, I'm a partial class!2";
+}
+
+#endif
---
+++ ModelTest/Program.cs
@@ -0,0 +1,8 @@
+﻿using Model;
+
+Console.WriteLine("Hello, World!");
+
+var model = new AppUser();
+
+Console.WriteLine(model.AuditName);
+
